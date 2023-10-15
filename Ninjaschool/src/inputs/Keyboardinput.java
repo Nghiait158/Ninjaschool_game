e700c28d50f5285bc5bb0ef41f37f2aee24263ca
Chun Nghia <ITITIU21254@student.hcmiu.edu.vn>
2023-10-15 14:20:41 +0700

    add move in keyboardInput

--- Ninjaschool/src/inputs/Keyboardinput.java
+++ Ninjaschool/src/inputs/Keyboardinput.java
@@ -3,23 +3,34 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import main.GamePanel;
+
 public class Keyboardinput implements KeyListener{
+
+    private GamePanel gamePanel;
+    public Keyboardinput(GamePanel gamePanel){
+        this.gamePanel=gamePanel;
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
         switch(e.getKeyCode()){
             case KeyEvent.VK_A:
-                System.out.println("A");
+                // System.out.println("A");
+                gamePanel.changeXDelta(-5);
                 break;
             case KeyEvent.VK_S:
-                System.out.println("S");
+                // System.out.println("S");
+                gamePanel.changeYDelta(5);
                 break;
             case KeyEvent.VK_D:
-                System.out.println("D");
+                // System.out.println("D");
+                gamePanel.changeXDelta(5);
                 break;
             case KeyEvent.VK_W:
-                System.out.println("W");
+                // System.out.println("W");
+                gamePanel.changeYDelta(-5);
                 break;
         }
     }
--- Ninjaschool/src/main/GamePanel.java
+++ Ninjaschool/src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 import java.awt.Graphics;
 import java.awt.event.KeyListener;
+import java.time.Year;
 
 import javax.swing.JPanel;
 
@@ -10,16 +11,27 @@ import inputs.Mouseinput;
 public class GamePanel extends JPanel {
     
     private Mouseinput mouseinput;
+    private int xDelta=0, yDelta=0;
     public GamePanel(){
 
         mouseinput= new Mouseinput();
-        addKeyListener(new Keyboardinput());
+        addKeyListener(new Keyboardinput(this));
         addMouseListener(mouseinput);
         addMouseMotionListener(mouseinput);
     }
+    public void changeXDelta(int value){
+        this.xDelta=value;
+        repaint();
+    }
+    public void changeYDelta( int value){
+        
+        this.yDelta=value;
+        repaint();
+    }   
+
     public void paintComponent (Graphics g){
         super.paintComponent(g);
 
-        g.fillRect(100, 100, 50, 50);
+        g.fillRect(100 + xDelta, 100 +yDelta, 50, 50);
     }
 }
